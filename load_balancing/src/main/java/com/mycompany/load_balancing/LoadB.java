5050b9c071c2689eece3817e11ce398480466bde
NegusNati <negusnati@gmail.com>
2023-05-03 12:16:35 -0700

    some clean up

--- load_balancing/src/main/java/com/mycompany/load_balancing/LoadB.java
+++ load_balancing/src/main/java/com/mycompany/load_balancing/LoadB.java
@@ -26,25 +26,24 @@ public class LoadB {
 
     public Server getNextServer() {
         Server server = servers.get(currentIndex);
-        currentIndex = (currentIndex + 1) % servers.size();
+        currentIndex = (currentIndex + 1) % servers.size(); //impleamenting round robin 
         return server;
     }
 
-    public void handleRequest(int port, String requestPath) {
+    public void handleRequest( String requestPath) {
         Server server = getNextServer();
-        port = 80;
+        int port = 80;
+        int serverCount = servers.size();
 
         try {
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < serverCount; i++) {
                 port = server.getPort();
             }
-            // To open a connection to the server
+            // To open a connection and query the server
 //        URL url = new URL("http://" + requestPath + ":" + port  );
             URL url = new URL("http://" + server.getIpAddress() + ":" + port);
-
-//            URL url = new URL("http://" + server.getIpAddress() + ":" + port + requestPath);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            System.out.println(" after connection");
+            System.out.println(" after connection is created");
             // Check if the server responded with a successful status code
             int statusCode = connection.getResponseCode();
             if (statusCode == HttpURLConnection.HTTP_OK) {
@@ -53,10 +52,10 @@ public class LoadB {
                 String response = "";
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
-                    response += inputLine;
+                    response += inputLine + "\n";
                 }
                 in.close();
-                System.out.println(" In If case");
+                System.out.println(" If all goes well");
                 System.out.println("Response from server " + server.getIpAddress() + ":" + port + ": " + response);
             } else {
                 System.out.println("Server " + server.getIpAddress() + ":" + port + " returned status code " + statusCode);
